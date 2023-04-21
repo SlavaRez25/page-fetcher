97ce56ca0f6aacc5ae757bc309b2aea84a9bb15e
Slava Reznik <reznik.slava@gmail.com>
2023-04-20 23:22:48 -0400

    Working code with comments and created new index.html file

--- fetcher.js
+++ fetcher.js
@@ -8,21 +8,26 @@ const fs = require('fs');
 const URL = process.argv[2];
 const locFilePath = process.argv[3];
 
-// using request library to check if URL is correct and if not log an error
-request(URL, function(error, response, body){
-  if (error){
-  console.log('error downloading file: ', error);
-  }
-});
-
-fs.writeFile(locFilePath, body, error => {
-  if (error) {
-    console.log('Error writing to file: ', locFilePath);
-  } else {
-    console.log('Downloaded')
-  }
-});
-
+// using request library to check if URL is correct and if not log an error and exit
+const grab = function(URL, locFilePath) { 
+  request(URL, function(error, response, body) {
+    if (error){
+      console.log('error downloading file: ', error);
+      return;  
+    }
+ 
+    // take the body length characters and write it to locFilePath file
+    fs.writeFile(locFilePath, body, error => {
+      if (error) {
+        console.log('Error writing to file: ', locFilePath);
+      } else {
+        console.log(`Downloaded and saved ${body.length} bytes to ${locFilePath}`);
+      }
+    });
+  });
+}
 console.log(URL);
 console.log(locFilePath);
 
+// grab function which sends the URL(HTTP) request, receives body from response and then write it to local file in your system
+grab(URL, locFilePath);
