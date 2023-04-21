5165eb4ac6b49fd8d185ffcaa6a3099312e30443
Slava Reznik <reznik.slava@gmail.com>
2023-04-20 20:27:14 -0400

    getting process.argv to work

---
+++ fetcher.js
@@ -0,0 +1,13 @@
+
+
+// const request = require('request');
+// const fs = requrie('fs');
+
+const URL = process.argv[2];
+const locFilePath = process.argv[3];
+
+console.log(process.argv);
+
+console.log(URL);
+console.log(locFilePath);
+
