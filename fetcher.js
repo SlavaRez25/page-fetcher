6f2400f306a73200cdbb8e9cdb274b695b0e8bcb
Slava Reznik <reznik.slava@gmail.com>
2023-04-20 22:59:20 -0400

    added request for the URL using request lib

--- fetcher.js
+++ fetcher.js
@@ -1,12 +1,27 @@
 
 
-// const request = require('request');
-// const fs = requrie('fs');
+const request = require('request');
+const fs = require('fs');
 
+
+// assigning the URL to the variable URL and the file path to the variable locFilePath
 const URL = process.argv[2];
 const locFilePath = process.argv[3];
 
-console.log(process.argv);
+// using request library to check if URL is correct and if not log an error
+request(URL, function(error, response, body){
+  if (error){
+  console.log('error downloading file: ', error);
+  }
+});
+
+fs.writeFile(locFilePath, body, error => {
+  if (error) {
+    console.log('Error writing to file: ', locFilePath);
+  } else {
+    console.log('Downloaded')
+  }
+});
 
 console.log(URL);
 console.log(locFilePath);
